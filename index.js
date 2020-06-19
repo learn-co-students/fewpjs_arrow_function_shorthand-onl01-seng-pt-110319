600312b732c3d58daac2c878d2acbbf86f0e8bce
Jordan <jordanleelong5@gmail.com>
2020-06-18 19:43:44 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+let divide = function() {
+    let num1 = 2000
+    let num2 = 100
+
+    let result = num1 / num2 
+    return result
+}
+
+let square = x => Math.pow(x, 2)
+
+let add = (a, b) => a + b
